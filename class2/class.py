d68438387f5448d7b71230459c0282e4b041821b
c1o0d0e1 <vpmy101@gmail.com>
2024-09-20 20:34:34 +0800

    class2

--- class2/class.py
+++ class2/class.py
@@ -1,1 +1,24 @@
 print('hello')
+# 字串運算
+a = 'hello'
+b = 'world'
+c = a + ' ' + b # 字串相加
+print(c)
+
+a = a * 3 # 字串乘法
+print(a)
+
+#認識基本指令
+#指令會有名稱跟括號組成，()裡面放提供給指令的參數
+#每個參數都要用逗號隔開
+m = max(1,2,3,4,5)
+print(m)
+a = 1
+b = 2
+m2 = min(a,b)
+le = len('hello') # 取長度
+print(le)
+print(type(1)) # 取型態
+print(type('hello')) # 取型態
+print(type(1.0)) # 取型態
+print(type(True)) # 取型態
