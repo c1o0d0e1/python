3e44ed23edfd5118a6c36ac6c64fb565b61197d2
c1o0d0e1 <vpmy101@gmail.com>
2024-11-15 19:17:45 +0800

    hw.py9(訂正版)

--- class9/hw.py
+++ class9/hw.py
@@ -1,17 +1,17 @@
-i = 1
-while i < 5:
-    juice = int(input("請輸入果汁編號: "))
-    if juice == 1:
-        print("您點的商品式蘋果汁")
-    elif juice == 2:
-        print("您點的商品式柳橙汁")
-    elif juice == 3:
-        print("您點的商品式葡萄汁")
-
-    try:
-        juice = int(input("請輸入果汁編號: "))
-    except:
-        print("輸入錯誤查無此果汁，請重新輸入")
+while True:
+    print("1. 蘋果汁")
+    print("2. 柳橙汁")
+    print("3. 葡萄汁")
+    print("4. 系統關閉")
+    type = input("請輸入編號: ")
+    if type == "1":
+        print("你點的商品是蘋果汁")
+    elif type == "2":
+        print("你點的商品是柳橙汁")
+    elif type == "3":
+        print("你點的商品是葡萄汁")
+    elif type == "4":
+        print("系統關閉")
+        break
     else:
-        print("~~系統關閉~~")
-        i = 5
+        print("輸入錯誤，請重新輸入")
