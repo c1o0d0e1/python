70c7e51e8804f1f8421acebd663dd706c38e1bbb
c1o0d0e1 <vpmy101@gmail.com>
2024-11-22 19:23:25 +0800

    hw.py訂正版

--- class10/hw.py
+++ class10/hw.py
@@ -3,15 +3,16 @@ import random
 start = 0
 end = 100
 m = random.randrange(1, 101)
-num = int(input("請輸入0~100的整數: "))
+num = 0
 while num != m:
+    num = int(input(f"請輸入{start}~{end}的整數: "))
     if num < m:
-        print("在大一點")
-        start = num
-        num = int(input(f"請輸入{start}~{end}的整數: "))
+        print("再大一點")
+        if num > start:
+            start = num
     elif num > m:
-        print("在小一點")
-        end = num
-        num = int(input(f"請輸入{start}~{end}的整數: "))
+        print("再小一點")
+        if num < end:
+            end = num
 
 print("恭喜猜中!")
