a459cef4f8ef891e2df6e630d556d4c3304393eb
c1o0d0e1 <vpmy101@gmail.com>
2024-12-06 19:46:17 +0800

    hw.py12(訂正版)

--- class12/hw.py
+++ class12/hw.py
@@ -1,31 +1,30 @@
 order = ["1. 新增餐點", "2. 刪除餐點", "3. 顯示餐點"]
-menu = []
 while True:
-    print(order)
-    try:
-        num = int(input("請輸入選擇: "))
-    except:
-        print("請輸入數字: ")
-        continue
-    else:
-        if num < 1 or num > 3:
-            print("輸入錯誤查無此選項，請重新輸入")
-            continue
-        else:
-            if num == 1:
-                name = input("請輸入餐點名稱: ")
-                menu.append(name)
-                print(menu)
-            elif num == 2:
-                name = input("請輸入要刪除的餐點名稱: ")
-                for i in range(menu.count(name)):
-                    menu.remove(name)
+    print(f"目前order有{order}")
+    print("1. 新增餐點")
+    print("2. 刪除餐點")
+    print("3. 顯示餐點")
 
-                print(menu)
-            elif num == 3:
-                for i in range(len(menu)):
-                    if menu.count(name) > 1:
-                        for j in range(menu.count(name)):
-                            menu.remove(name)
-                    else:
-                        print(f"{menu[i]}:{menu.count(menu[i])}")
+    choice = input("請選擇功能: ")
+
+    if choice == "1":
+        item = input("請輸入餐點名稱: ")
+        order.append(item)
+    elif choice == "2":
+        item = input("請輸入要刪除的餐點名稱: ")
+        if item in order:
+            for i in range(order.count(item)):
+                order.remove(item)
+    elif choice == "3":
+        items = input("請輸入餐點名稱: ")
+        for x in order:
+            if x not in items:
+                items.append(x)
+
+        for x in items:
+            print(f"{x}:{order.count(x)}")
+
+        break
+
+    else:
+        print("無效的選擇，請重新輸入!")
