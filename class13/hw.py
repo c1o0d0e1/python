39ad97962d5929ca1e5dc7e4f0c1300b673f2d85
c1o0d0e1 <vpmy101@gmail.com>
2024-12-13 17:00:19 +0800

    hw.py12

--- class13/hw.py
+++ class13/hw.py
@@ -1,1 +1,6 @@
 food_list = {"蛋糕": 1, "三明治": 10, "果汁": 20, "薯條": 15, "披薩": 15}
+for k, v in food_list.items():
+    if k == "果汁":
+        print(f"{k}: {v}杯")
+    else:
+        print(f"{k}: {v}份")
