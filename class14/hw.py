1e1ea0a86a420e9639db8cf76f2b48d0b7b61793
c1o0d0e1 <vpmy101@gmail.com>
2024-12-14 23:37:57 +0800

    hw.py14(更新版)

--- class14/hw.py
+++ class14/hw.py
@@ -3,30 +3,41 @@ while True:
     print("1. 新增科目成績")
     print("2. 刪除科目成績")
     print("3. 提交所有成績並顯示平均")
-    m = input("請選擇功能: ")
+    m = input("請選擇功能編號: ")
     print("=================================")
     if m == "1":
-        subject = input("請輸入科目新增的名稱: ")
+        subject = input("請輸入新增的科目: ")
         try:
-            score = int(input("請輸入成績: "))
+            score = int(input("請輸入分數: "))
         except:
             print("輸入錯誤，請重新輸入")
             score = int(input("請輸入分數: "))
             print("=================================")
-            continue
+            d[subject] = score
+            for i in d:
+                print(f"{i}: {d[i]}")
         else:
             d[subject] = score
-            print(d)
+            print("=================================")
+            for i in d:
+                print(f"{i}: {d[i]}")
     elif m == "2":
-        subject = input("請輸入要刪除的科目名稱: ")
+        subject = input("請輸入想刪除的科目: ")
         try:
             d.pop(subject)
         except:
             print("此科目尚未新增!")
-            print(d)
+            print("=================================")
+            for i in d:
+                print(f"{i}: {d[i]}")
         else:
-            print(d)
+            print("刪除成功!")
+            print("=================================")
+            for i in d:
+                print(f"{i}: {d[i]}")
     elif m == "3":
+        for i in d:
+            print(f"{i}: {d[i]}")
         total = 0
         for i in d:
             total += d[i]
