8c35f0189e2e9061f95b43618dc5baafc5aacc31
c1o0d0e1 <vpmy101@gmail.com>
2024-12-20 19:43:00 +0800

    hw.py14(訂正版)

--- class14/hw.py
+++ class14/hw.py
@@ -1,45 +1,41 @@
-d = {}
+score_dict = {}
 while True:
+    for d, score in score_dict.items():
+        print(f"{subject}: {score}")
     print("1. 新增科目成績")
     print("2. 刪除科目成績")
     print("3. 提交所有成績並顯示平均")
-    m = input("請選擇功能編號: ")
+    choice = input("請選擇功能編號: ")
     print("=================================")
-    if m == "1":
+    if choice == "1":
+
         subject = input("請輸入新增的科目: ")
-        try:
-            score = int(input("請輸入分數: "))
-        except:
-            print("輸入錯誤，請重新輸入")
-            score = int(input("請輸入分數: "))
-            print("=================================")
-            d[subject] = score
-            for i in d:
-                print(f"{i}: {d[i]}")
+        if subject in score_dict:
+            print("此科目已經新增過了!")
         else:
-            d[subject] = score
-            print("=================================")
-            for i in d:
-                print(f"{i}: {d[i]}")
-    elif m == "2":
+            while True:
+                try:
+                    score = int(input("請輸入分數: "))
+                    score_dict[subject] = score
+                    break
+                except:
+                    print("輸入錯誤，請重新輸入")
+
+    elif choice == "2":
         subject = input("請輸入想刪除的科目: ")
-        try:
-            d.pop(subject)
-        except:
-            print("此科目尚未新增!")
-            print("=================================")
-            for i in d:
-                print(f"{i}: {d[i]}")
-        else:
+        if subject in score_dict:
+            score_dict.pop(subject)
             print("刪除成功!")
-            print("=================================")
-            for i in d:
-                print(f"{i}: {d[i]}")
-    elif m == "3":
-        for i in d:
-            print(f"{i}: {d[i]}")
-        total = 0
-        for i in d:
-            total += d[i]
-        print(f"平均成績為{total / len(d)}")
+        else:
+            print("此科目尚未新增!")
+
+    elif choice == "3":
+        if len(score_dict) == 0:
+            print("目前沒有登記成績!")
+        else:
+            for subject, score in score_dict.items():
+                print(f"{subject}: {score}")
+            print(f"總平均為{sum(score_dict.values()) / len(score_dict)}")
         break
+    else:
+        print("查無此功能，請重新輸入")
